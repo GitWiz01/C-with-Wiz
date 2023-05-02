1d2ae9946ee15fd7bf579d77a772617b91b7cfa9
Abhishek Parmar <helloworldviz@gmail.com>
2023-05-02 21:30:56 +0530

    updated

--- Palindrome-check.c
+++ Palindrome-check.c
@@ -1,22 +1,24 @@
-//           ------check whether a number is palindrome or not------             
+//           ------check whether a number is palindrome or not------
 //        A palindromic number (also known as a numeral palindrome or a numeric palindrome) is a number (such as 16461) that remains the same when its digits are reversed.
 
 #include <stdio.h>
 
 int main()
 {
-    int i, n, rem, rev, pali=0;
+    int i, n, rem, original, rev = 0;
 
     printf("Enter the number\n");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    while(n!=0)
+    original = n;
+    
+    while (n != 0)
     {
-        rem=n%10;
-        n = n/10;
-        rev= rev*10 + rem;
+        rem = n % 10;
+        n = n / 10;
+        rev = rev * 10 + rem;
     }
-    if(rev==n)
+    if (rev == original)
     {
         printf("Entered number is a palindrome\n");
     }
@@ -24,6 +26,6 @@ int main()
     {
         printf("Entered number is not a palindrome\n");
     }
+
     return 0;
 }
-
