34a9e031088ba2043e21ef04f37ca1b90225059d
Abhishek Parmar <helloworldviz@gmail.com>
2023-05-03 23:28:39 +0530

    update

--- Doc1.c
+++ Doc1.c
@@ -9,19 +9,27 @@ int main()
     float C;
     double D;
 
-    printf("enter any Integer\n");
-    scanf("%d", &A);
+    printf("enter any Integer\n"); 
+    scanf("%d", &A);                   //   %d is a format specifier used for integer
     printf("%d\n", A);
+
     printf("enter any Character\n");
-    scanf("%c", &B);
+    scanf("%c", &B);                   //   %c is a format specifier used for character
     printf("%c\n", B);
+
     printf("enter any decimal\n");
-    scanf("%f", &C);
+    scanf("%f", &C);                    //   %f is a format specifier used for decimal
     printf("%f\n", C);
+
     printf("enter any decimal\n");
-    scanf("%lf", &D);
+    scanf("%lf", &D);                   //   %lf is a format specifier used for large decimal
     printf("%lf\n", D);
     
 
     return (0);
 }
+/*  NOTE ---->  However,%f is a format specifier for float but here we wrote decimal
+                well both have a small difference that is, float gives a approximate 
+                value whereas decimal gives an exact value.  */
+
+
