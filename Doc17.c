385adcec070515f8c632c227bba625b329ca1f45
Abhishek Parmar <helloworldviz@gmail.com>
2023-10-03 16:54:16 +0530

    updated

---
+++ Doc17.c
@@ -0,0 +1,16 @@
+//                        ------fibonacci series for n terms------
+
+#include<stdio.h>
+void main()
+{
+	int i,n,a=0,b=1,c;
+	printf("Enter number=");	
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\n",a);
+		c=a+b;
+		a=b;
+		b=c;	
+	}		
+}
