32ec31fb65c8fc5a0e3182b3061f07b09d595418
saisreesatya <132558766+saisreesatyassss@users.noreply.github.com>
2023-12-30 14:12:31 +0530

    added comments for most of the code which not have commets

--- Doc14.c
+++ Doc14.c
@@ -2,41 +2,56 @@
 
 #include<stdio.h>
 
+// Entry point of the program
 void main() 
 {
-	int hindi, english, science,math,computer,sum ;
-	float per;
-	printf("Enter marks of Hindi=");
-	scanf("%d",&hindi);
-	printf("Enter marks of English=");
-	scanf("%d",&english);
-	printf("Enter marks of Science=");
-	scanf("%d",&science);
-	printf("Enter marks of Math=");
-	scanf("%d",&math);
-	printf("Enter marks of Computer=");
-	scanf("%d",&computer);
-	
-	sum=hindi+english+science+math+computer;
-	printf("\nSum of marks=%d",sum);
-	
-	per=(float)sum/5;
-	printf("\nPercentage of marks=%f",per);
-	
-	if(per>=90&&per<=100)
-	{
-		printf("\nGrade A");
-	}
-	else if(per>=80&&per<90)
-	{
-		printf("\nGrade B");
-	}
-	else if(per>=60&&per<80)
-	{
-		printf("\nGrade C");
-	}
-	else if(per<60)
-	{
-		printf("\nGrade D");
-	}
+    // Declare variables for marks in different subjects, total marks, and percentage
+    int hindi, english, science, math, computer, sum;
+    float per;
+
+    // Prompt user to enter marks for each subject
+    printf("Enter marks of Hindi: ");
+    scanf("%d", &hindi);
+
+    printf("Enter marks of English: ");
+    scanf("%d", &english);
+
+    printf("Enter marks of Science: ");
+    scanf("%d", &science);
+
+    printf("Enter marks of Math: ");
+    scanf("%d", &math);
+
+    printf("Enter marks of Computer: ");
+    scanf("%d", &computer);
+
+    // Calculate the total marks
+    sum = hindi + english + science + math + computer;
+
+    // Display the sum of marks
+    printf("\nSum of marks: %d", sum);
+
+    // Calculate the percentage
+    per = (float)sum / 5;
+
+    // Display the percentage
+    printf("\nPercentage of marks: %f", per);
+
+    // Determine and display the grade based on the percentage
+    if (per >= 90 && per <= 100)
+    {
+        printf("\nGrade A");
+    }
+    else if (per >= 80 && per < 90)
+    {
+        printf("\nGrade B");
+    }
+    else if (per >= 60 && per < 80)
+    {
+        printf("\nGrade C");
+    }
+    else if (per < 60)
+    {
+        printf("\nGrade D");
+    }
 }
--- Doc9.c
+++ Doc9.c
@@ -2,18 +2,26 @@
 
 #include<stdio.h>
 
+// Entry point of the program
 void main()
 {
-	int  a,b,temp;
-	printf("Enter a=");
-	scanf("%d",&a);
-	printf("Enter b=");
-	scanf("%d",&b);
-	
-	temp=a;
-	a=b;
-	b=temp;
-	printf("\nAfter swapping");
-	printf("\na=%d",a);
-	printf("\nb=%d",b);
+    // Declare variables for two input numbers and a temporary variable for swapping
+    int a, b, temp;
+
+    // Prompt user to enter values for a and b
+    printf("Enter a: ");
+    scanf("%d", &a);
+
+    printf("Enter b: ");
+    scanf("%d", &b);
+
+    // Swap the values of a and b using a temporary variable
+    temp = a;
+    a = b;
+    b = temp;
+
+    // Display the values after swapping
+    printf("\nAfter swapping");
+    printf("\na = %d", a);
+    printf("\nb = %d", b);
 }
